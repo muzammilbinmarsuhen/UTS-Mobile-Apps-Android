66fbba6cbb0a7afeb85c8f30cc5417d849a0e360
muzammilbinmarsuhen <j7958962@gmail.com>
2024-05-04 21:26:28 +0700

    penambahan file akun dan pengeditan di folder screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,68 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from './screens/Home';
+import Pesanan from './screens/Pesanan';
+import Akun from './screens/Akun';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const MenuTab = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Tabs.Navigator screenOptions={{tabBarActiveTintColor: 'green'}}>
+      <Tabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Pesanan"
+        component={Pesanan}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons
+              name="note-text-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Akun"
+        component={Akun}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
   );
 };
 
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={MenuTab}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default App;
 
-const styles = StyleSheet.create({});
+const style = StyleSheet.create({});
---
+++ src/screens/Home.jsx
@@ -0,0 +1,46 @@
+import {StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import React from 'react';
+import IconMenu from '../components/IconMenu';
+
+const Data = [
+  {id: 1, label: 'Top Up'},
+  {id: 2, label: 'Diskon'},
+  {id: 3, label: 'Go Food'},
+  {id: 4, label: 'Grab'},
+  {id: 5, label: 'Gojek'},
+];
+
+const Home = ({navigation}) => {
+  return (
+    <View>
+      <FlatList
+        data={Data}
+        renderItem={({item}) => <IconMenu label={item.label} />}
+        horizontal={true}
+      />
+      <TouchableOpacity
+        onPress={() => navigation.navigate('About')}
+        style={styles.btn}>
+        <Text style={styles.txt}>Ke About</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  btn: {
+    margin: 10,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: 'green',
+  },
+  txt: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
